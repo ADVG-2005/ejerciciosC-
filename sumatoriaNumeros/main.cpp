b6635e201381e830f57a7d29e5c3630eb1565531
advg <advguwu.09.02.2005@gmail.com>
2024-12-04 18:23:30 -0500

    Creo el main e importo la cabecera "funciones.hpp" para ejecutar la funcion creada.

---
+++ sumatoriaNumeros/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "funciones.hpp"
+using namespace std;
+
+int main(){
+    int n;
+    cout <<"Ingrese un numero : ";
+    cin >>n ;
+    sumatoriaRecursiva(n);
+    return 0;
+}
