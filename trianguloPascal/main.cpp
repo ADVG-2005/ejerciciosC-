0c1e5441c6f3d5bc4ff206176327a25d60a31b24
advg <advguwu.09.02.2005@gmail.com>
2024-12-05 22:36:22 -0500

    main para correr la funcion triangulo de pascal

---
+++ trianguloPascal/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "funciones.hpp"
+using namespace std;
+
+int main(){
+    int filas;
+    cout <<"Ingrese la cantidad de Filas: ";
+    cin >>filas;
+    trianguloPascal(filas);
+
+
+    return 0;
+}
